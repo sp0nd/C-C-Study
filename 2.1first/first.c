b752d94e522a02e40609f90c4cca394d44fddb49
sp0nd <67685987+sp0nd@users.noreply.github.com>
2020-10-17 14:46:02 +0900

    Add files via upload

---
+++ 2.1first/first.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(void)				        /* a simple profram				*/
+
+{
+	int num;						/* define a variavle called num */
+	num = 1;						/* assign a value to num		*/
+	int a;
+	printf("I am a simple");		/* use the printf() function	*/
+	printf("computer.\n");
+	printf("My favorite numver is %d because it is first. \n", num);
+	scanf("%d",&a);
+
+	system("pause");
+
+	return 0;
+}
+
+
